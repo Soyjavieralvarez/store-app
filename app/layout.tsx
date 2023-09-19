f3ed7d2a7d0a1edf06fd3307b83fa5eb2e88759f
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-19 20:55:06 +0200

    navbar categories done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
-import Footer from '@/components/footer'
-import './globals.css'
-import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
+
+import Footer from '@/components/footer'
+import Navbar from "@/components/navbar"
+import Container from "@/components/ui/container"
+
+
+import './globals.css'
+
+import type { Metadata } from 'next'
+
+
 
 const font = Urbanist({
   subsets: ['latin'],
@@ -21,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Navbar />
         {children}
         <Footer />
         </body>
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,12 @@
 const Footer = () => {
     return (
         <footer className="bg-white border-t">
-            
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs border-black">
+                    &copy; 2023 Javier Álvarez Store, Inc. All rights reserved.
+                    {/* <a href="https://www.linkedin.com/in/javier-alvarez-gonzalez-desarrollo/" target="_blank"><p className="text-blue-900 underline">LinkedIn profile</p></a> */}
+                </p>
+            </div>
         </footer>
     );
 }
