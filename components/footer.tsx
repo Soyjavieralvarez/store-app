94596c437881c027b4ea5ab2c1fdcda3d0a29953
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-20 20:29:25 +0200

    icon-buttons & product-card created

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,8 @@ const Footer = () => {
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs border-black">
-                    &copy; 2023 Javier Álvarez Store, Inc. All rights reserved.
-                    <a href="https://www.linkedin.com/in/javier-alvarez-gonzalez-desarrollo/" target="_blank"><p className="text-blue-900 underline">LinkedIn profile</p></a>
+                    &copy; 2023 <a href="https://www.linkedin.com/in/javier-alvarez-gonzalez-desarrollo/" target="_blank">Javier Álvarez Store, Inc. All rights reserved.</a>
+                    {/* <a href="https://www.linkedin.com/in/javier-alvarez-gonzalez-desarrollo/" target="_blank"><p className="text-blue-900 underline">LinkedIn profile</p></a> */}
                 </p>
             </div>
         </footer>
