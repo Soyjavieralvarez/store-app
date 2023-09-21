bc30a6231888b82445b04cbef4a312390149d4d1
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-21 21:21:09 +0200

    gallery, gallery-tab and index created

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -2,6 +2,8 @@ import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
+import Gallery from "@/components/ui/gallery/index";
+
 
 interface ProductPageProps {
     params: {
@@ -24,15 +26,13 @@ const ProductPage: React.FC <ProductPageProps> = async ({
             <Container>
                 <div className="px-4 py-10 sm:px-6 lg:px-8">
                     <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                        {/* Gallery */}
-                        <div>Gallery</div>
+                        <Gallery images={product.images}/>
                         <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                        {/* Info */}
-                        Info
+                            <Info data={product}/>
                         </div>
                     </div>
                     <hr className="my-10"/>
-                    <ProductList title="Related Items" items={suggestedProducts} />
+                    <ProductList title="Productos relacionados" items={suggestedProducts} />
                 </div>
             </Container>
             Individual Product
