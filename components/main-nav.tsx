b152ea03bb307bfda4cb338d58cabdaecc3b3116
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-24 18:31:22 +0200

    mainnavbar from mobile phone created

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import { Category } from "@/types";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 interface MainNavProps {
     data: Category[];
@@ -15,30 +16,61 @@ const MainNav: React.FC<MainNavProps> = ({
 
     const pathname = usePathname();
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
+
     const routes = data.map((route) => ({
         href: `/category/${route.id}`,
         label: route.name,
         active: pathname === `/category/${route.id}`,
     }))
 
-  return (
-    <nav
-    className="mx-6 flex items-center space-x-4 lg:space-x-6"
-    >
-        {routes.map((route) => (
+    return (
+      <>
+        {/* Botón de menú para dispositivos móviles */}
+        <button
+          className="lg:hidden"
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle Menu"
+        >
+          ☰
+        </button>
+  
+        {/* Menú para dispositivos móviles */}
+        {menuOpen && (
+          <nav className="lg:hidden">
+            {routes.map((route) => (
+              <Link
+                key={route.href}
+                href={route.href}
+                className={cn(
+                  "text-sm font-medium transition-colors hover:text-black m-2",
+                  route.active ? "text-black" : "text-neutral-500"
+                )}
+              >
+                {route.label}
+              </Link>
+            ))}
+          </nav>
+        )}
+  
+        {/* Menú para ordenadores */}
+        <nav className="hidden m-6 lg:flex items-center space-x-4 lg:space-x-6">
+          {routes.map((route) => (
             <Link
-            key={route.href}
-            href={route.href}
-            className={cn(
+              key={route.href}
+              href={route.href}
+              className={cn(
                 "text-sm font-medium transition-colors hover:text-black",
                 route.active ? "text-black" : "text-neutral-500"
-            )}
+              )}
             >
-            {route.label}
+              {route.label}
             </Link>
-        ))}
-    </nav>
-  )
-}
+          ))}
+        </nav>
+      </>
+    );
+  };
 
 export default MainNav;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+
+
 import Link from "next/link"
 
 import Container from "@/components/ui/container"
@@ -12,13 +14,15 @@ export const revalidate = 0;
 const Navbar = async () => {
 
   const categories = await getCategories();
+
+  
   
   return (
     <div className="border-b">
         <Container>
-          <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <div className=" relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-           <p className="font-bold text-xl">Tienda</p>
+           <p className="font-bold text-xl m-6">Tienda</p>
            </Link>
            <MainNav data={categories} />
            <NavbarActions />
