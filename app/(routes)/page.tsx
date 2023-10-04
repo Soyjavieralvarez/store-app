a506a26969da87d5b3e75f174f379ffed92e6932
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-10-04 18:25:42 +0200

    products fixed

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,29 +1,27 @@
 import getBillboard from "@/actions/get-billboard";
-import Container from "@/components/ui/container";
-import Billboard from "@/components/billboard";
 import getProducts from "@/actions/get-products";
 import ProductList from "@/components/product-list";
-
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
-    const products = await getProducts({ isFeatured: true});
-    const billboard = await getBillboard("f535ded9-9239-4c8e-86b5-cf28baa98163");
-    return (
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("f535ded9-9239-4c8e-86b5-cf28baa98163");
 
+  return (
     <Container>
-        <div className="space-y-10 pb-10">
-            <Billboard data={billboard}/>
-        
+      <div className="space-y-10 pb-10">
+        <Billboard 
+          data={billboard}
+        />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-            <ProductList title="Productos destacados" items={products} />
+          <ProductList title="Featured Products" items={products} />
         </div>
-        </div>
+      </div>
     </Container>
-
-    );
-    
-}
+  )
+};
 
 export default HomePage;
